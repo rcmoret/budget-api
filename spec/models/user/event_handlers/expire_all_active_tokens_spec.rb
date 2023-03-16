b1c00cb3a60bc120039583632e9d414a939fa921
Ryan's Mac Mini <ryancmoret@gmail.com>
2023-03-16 05:16:29 -0500

    wip

---
+++ spec/models/user/event_handlers/expire_all_active_tokens_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe User::EventHandlers::ExpireAllActiveTokens do
+  describe "#call" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:event) { FactoryBot.create(:user_event, actor: user) }
+    let(:success_event) { User::EventType.for(:user_expired_all_tokens) }
+
+    before { FactoryBot.create_list(:auth_token_context, 5, user: user) }
+
+    it "expires all active tokens for a user" do
+      expect { described_class.new(event).call }
+        .to(change { Auth::Token::Context.active.belonging_to(user).count }
+        .from(5)
+        .to(0))
+    end
+
+    it "records a success event" do
+      expect { described_class.new(event).call }
+        .to(change { User::Event.where(actor: user, event_type: success_event).count }.by(+1))
+    end
+  end
+end
