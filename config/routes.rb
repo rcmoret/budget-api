23d5be722a7b6a37fb5cb042f2869e5566a8b3c6
Ryan Moret <ryancmoret@gmail.com>
2023-03-19 11:34:59 -0600

    Added to shared examples

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :accounts do
       get "/", to: "index#call"
+      post "/", to: "create#call"
+      put "/:key", to: "update#call"
+      delete "/:key", to: "delete#call"
+
+      scope "/:account_key/transactions/:month/:year", module: :transactions do
+        get "/", to: "index#call"
+        post "/", to: "create#call"
+        put "/:key", to: "update#call"
+        delete "/:key", to: "delete#call"
+      end
     end
 
     post "/tokens", to: "token_create#call"
