b3ce974ef2fd5f0b7dd6a0425da2df6963bf9178
Ryan Moret <ryancmoret@gmail.com>
2023-04-10 19:23:50 -0600

    Restrict fetchable to only accept keys

--- app/models/concerns/fetchable.rb
+++ app/models/concerns/fetchable.rb
@@ -4,14 +4,8 @@ module Fetchable
   extend ActiveSupport::Concern
 
   class_methods do
-    def fetch(user:, slug: nil, key: nil)
-      raise ArgumentError, "must provide either slug or key" if slug.nil? && key.nil?
-
-      if key.present?
-        belonging_to(user).by_key(key)
-      else
-        belonging_to(user).by_slug(slug)
-      end
+    def fetch(user:, key:)
+      belonging_to(user).by_key(key)
     end
   end
 end
