131b1c6d6a6f830f4e2df81bcb48e24da3e963c5
Ryan's Mac Mini <ryancmoret@gmail.com>
2024-02-24 11:52:06 -0600

    Extract available cash to a mixin

--- app/serializers/budget/intervals/discretionary_serializer.rb
+++ app/serializers/budget/intervals/discretionary_serializer.rb
@@ -1,6 +1,8 @@
 module Budget
   module Intervals
     class DiscretionarySerializer < ApplicationSerializer
+      include Mixins::AvailableCash
+
       attribute :amount
       attribute :over_under_budget
       attribute :transaction_details, on_render: :render
@@ -35,34 +37,6 @@ module Budget
 
       def items
         super.map(&:decorated)
-      end
-
-      def available_cash
-        cash_flow_transaction_detail_scope
-          .or(non_cash_flow_budget_inclusion_scope)
-          .joins(:account)
-          .merge(Account.belonging_to(user_group))
-          .joins(:details)
-          .sum(:amount)
-      end
-
-      def cash_flow_transaction_detail_scope
-        Transaction::Entry.cash_flow.then do |scope|
-          if future?
-            scope.between(date_range)
-          else
-            scope.prior_to(last_date, include_pending: current?)
-          end
-        end
-      end
-
-      def non_cash_flow_budget_inclusion_scope
-        Transaction::Entry
-          .between(date_range, include_pending: current?)
-          .where(
-            budget_exclusion: false,
-            account_id: Account.belonging_to(user_group).non_cash_flow.select(:id)
-          )
       end
     end
   end
