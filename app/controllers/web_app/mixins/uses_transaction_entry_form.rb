a36d6a41070f99310bf1d47a0665b637ab47e544
RyanMoret <rmoret@investwithroots.com>
2024-11-06 18:31:18 -0600

    Add route for transaction create
    * Add corresponding controller
      * add a mixin and move shared behavior
        to a mixin for shared behavior

--- app/controllers/web_app/mixins/uses_transaction_entry_form.rb
+++ app/controllers/web_app/mixins/uses_transaction_entry_form.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 module WebApp
-  module Transactions
-    class UpdateController < BaseController
+  module Mixins
+    module UsesTransactionEntryForm
+      extend ActiveSupport::Concern
+
       include Mixins::HasRedirectParams
       include Mixins::HasBudgetInterval
       include Mixins::HasAccount
 
-      DEFAULT_TRANSACTION_ENTRY_PERMITTED_PARAMS = %i[
+      BASE_TRANSACTION_ENTRY_PERMITTED_PARAMS = %i[
         is_budget_exclusion
         check_number
         clearance_date
@@ -16,13 +18,11 @@ module WebApp
         receipt
       ].freeze
 
-      DEFAULT_TRANSACTION_DETAIL_PARAMS = %i[
+      BASE_TRANSACTION_DETAIL_PARAMS = %i[
         key
         amount
         budget_item_key
       ].freeze
-
-      before_action -> { redirect_to(home_path) }, if: -> { transaction.nil? }
 
       def call
         if transaction_form.save
@@ -38,20 +38,16 @@ module WebApp
         @transaction_form ||= Forms::TransactionForm.new(current_user_profile, transaction, form_parameters)
       end
 
-      def transaction
-        @transaction ||= account.transactions.by_key(params[:key])
+      def handle_detail(detail_attrs)
+        detail_id = transaction.details.by_key(detail_attrs.fetch("key"))&.id
+        budget_item = budget_item_look_up(detail_attrs.delete("budget_item_key"))
+        detail_attrs.merge(id: detail_id, budget_item: budget_item)
       end
 
-      def transaction_entry_permitted_params
-        DEFAULT_TRANSACTION_ENTRY_PERMITTED_PARAMS.dup + %i[account_key]
-      end
-
-      def transaction_details_permitted_params
-        DEFAULT_TRANSACTION_DETAIL_PARAMS.dup + %i[_destroy]
-      end
-
-      def permitted_parameters
-        transaction_entry_permitted_params << { details_attributes: transaction_details_permitted_params }
+      def form_parameters
+        @form_parameters ||= formatted_params.reduce({}) do |memo, (key, value)|
+          memo.merge(handle_attribute(key, value))
+        end
       end
 
       def formatted_params
@@ -59,12 +55,6 @@ module WebApp
           .require(:transaction)
           .permit(*permitted_parameters)
           .to_h
-      end
-
-      def form_parameters
-        @form_parameters ||= formatted_params.reduce({}) do |memo, (key, value)|
-          memo.merge(handle_attribute(key, value))
-        end
       end
 
       # rubocop:disable Metrics/CyclomaticComplexity
@@ -82,19 +72,11 @@ module WebApp
       end
       # rubocop:enable Metrics/CyclomaticComplexity
 
-      def handle_detail(detail_attrs)
-        detail_id = transaction.details.by_key(detail_attrs.fetch("key"))&.id
-        budget_item = budget_item_look_up(detail_attrs.delete("budget_item_key"))
-        detail_attrs.merge(id: detail_id, budget_item: budget_item)
-      end
-
       def budget_item_look_up(budget_item_key)
         return if budget_item_key.nil?
 
         ::Budget::Item.fetch(current_user_profile, key: budget_item_key)
       end
-
-      def namespace = "accounts"
     end
   end
 end
