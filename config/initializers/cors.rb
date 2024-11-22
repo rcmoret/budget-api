1c8a9a4217c003385a0342dae01df5b7b13196e3
RyanMoret <rmoret@investwithroots.com>
2024-11-22 17:21:32 -0600

    Comment out cors initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
-Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
-  if Rails.env.development? || Rails.env.test?
-    allow do
-      origins "*"
-      resource "/api/*", headers: :any, methods: %i[get post patch put delete]
-    end
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   if Rails.env.development? || Rails.env.test?
+#     allow do
+#       origins "*"
+#       resource "/api/*", headers: :any, methods: %i[get post patch put delete]
+#     end
+#   end
+# end
